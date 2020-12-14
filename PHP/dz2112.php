e58d79b1e6d06d37bf2cdc432711d577c2f87ec7
Papkxo87 <74614709+Papkxo87@users.noreply.github.com>
2020-12-14 22:15:10 +0300

    капитализация

---
+++ PHP/dz2112.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $a = 100;//сумма вклада
+    $b = 5;//процент годовых
+    $x = 10;//количество месяцев
+    $y = ($a*$b)/$x
+    echo $y . '<br>';
+
+   
+    ?>
+</body>
+</html>
