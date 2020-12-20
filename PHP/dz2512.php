f7e898905478ea49ce977bf97d2d6bfc6b0285a4
Papkxo87 <74614709+Papkxo87@users.noreply.github.com>
2020-12-20 16:32:19 +0300

    +for

---
+++ PHP/dz2512.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    //Фибоначчи c for
+   /* $z = 25;
+    $a = 1;
+    for($i=1; $i<$z; $i+=$a) {
+    echo $i, ' ';
+    $a = $i - $a;
+    }*/
+    
+
+    $z = 30;
+    $s = 0;
+    $i = 1;
+    while ($i <= $z) {
+        $s = $i;
+        $S+= $i;
+        echo $i . '  ';
+    }
+    
+   
+    ?>
+</body>
+
+</html>
