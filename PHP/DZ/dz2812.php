c2173b3c1f0d0b5bbfaa083482bb3ceed10a1e2a
Papkxo87 <74614709+Papkxo87@users.noreply.github.com>
2020-12-24 01:23:14 +0300

    готовый тест по географии

--- PHP/DZ/dz2812.php
+++ PHP/DZ/dz2812.php
@@ -1,15 +1,42 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>REZULTAT</title>
+    <style type="text/css">
+    body{
+        background-image: url(compass.jpg);
+        background-size: cover;
+        text-align: center;
+        padding-top:300px;
+        font-size: 35px;
+        color: black;
+    }
+    
+    </style>
+</head>
+<body>
+    <?php
+$sum = 0;
+$sum += $_POST['fo1'] == 5;
+$sum += $_POST['fo2'] == "Джомолургма";
+$sum += $_POST['fo3'] == "Токио";
+$sum += $_POST['fo4'] == "Байкал";
+$sum += $_POST['fo5'] == "Нил";
+$sum += $_POST['fo6'] == "Сахара";
+$sum += $_POST['fo7'] == "Ватикан";
+$sum += $_POST['fo8'] == "Австралия";
+$sum += $_POST['fo9'] == "Евразия";
+$sum += $_POST['fo10'] == "Россия";
 
-$right = 0;
+if ($sum >= 5) {
+    echo "ПОЗДРАВЛЯЕМ! ВЫ ЗНАЕТЕ ГЕОГРАФИЮ";
+} else {
+    echo "НЕ РАССТРАИВАЙТЕСЬ. В СЛЕДУЮЩИЙ РАЗ У ВАС ВСЁ ПОЛУЧИТСЯ";
+}
+?>
+</body>
+</html>
 
-$right += $_POST['fo1'] == 35;
-$right += $_POST['fo2'] == 56;
-$right += $_POST['fo3'] == 70;
-$right += $_POST['fo4'] == 45;
-$right += $_POST['fo5'] == 125;
-$right += $_POST['fo6'] == 16;
-$right += $_POST['fo7'] == 81;
-$right += $_POST['fo8'] == 42;
-$right += $_POST['fo9'] == 27;
-$right += $_POST['fo10'] == 91;
-echo $right;
+
