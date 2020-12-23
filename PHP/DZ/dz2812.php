bd0abea6fa049ca3e63ad1dc89c293d814837404
Papkxo87 <74614709+Papkxo87@users.noreply.github.com>
2020-12-24 00:02:39 +0300

    test v html

---
+++ PHP/DZ/dz2812.php
@@ -0,0 +1,15 @@
+<?php
+
+$right = 0;
+
+$right += $_POST['fo1'] == 35;
+$right += $_POST['fo2'] == 56;
+$right += $_POST['fo3'] == 70;
+$right += $_POST['fo4'] == 45;
+$right += $_POST['fo5'] == 125;
+$right += $_POST['fo6'] == 16;
+$right += $_POST['fo7'] == 81;
+$right += $_POST['fo8'] == 42;
+$right += $_POST['fo9'] == 27;
+$right += $_POST['fo10'] == 91;
+echo $right;
